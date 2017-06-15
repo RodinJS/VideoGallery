3114bb20fa329f4c30a00e26f4424ba1b61749e9
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-06-15 17:13:32 +0400

    RO-918 # gazepoint for mobiles

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ RODIN.start();
  */
 let mainContainer = new MainContainer();
 mainContainer.run();
+
+RODIN.messenger.on(RODIN.CONST.ACTIVE_SCENE, () => {
+    if(RODIN.device.isMobile && RODIN.device.isVR) {
+        RODIN.Scene.HMDCamera.add(RODIN.GamePad.cardboard.gazePoint.Sculpt);
+    }
+});
